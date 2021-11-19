95ff684c49a49d78ef61911bbf0feaef7b731214
David Sandoval <david.sandoval@wolox.co>
2021-11-19 17:19:23 -0500

    fix suggestions
    add invalid user error handling

--- app/errors.js
+++ app/errors.js
@@ -11,3 +11,6 @@ exports.defaultError = message => internalError(message, exports.DEFAULT_ERROR);
 
 exports.REQUEST_ERROR = 'request_error';
 exports.requestError = message => internalError(message, exports.REQUEST_ERROR);
+
+exports.REQUEST_ERROR = 'invalid_user';
+exports.invalidUser = message => internalError(message, exports.REQUEST_ERROR);
--- app/helpers/authentication.js
+++ app/helpers/authentication.js
@@ -1,12 +1,6 @@
 const bcrypt = require('bcrypt');
 const bcryptConfig = require('../../config').common.bcrypt;
 
-exports.hash = async password => {
-  const hash = await bcrypt.hash(password, parseInt(bcryptConfig.salt));
-  return hash;
-};
+exports.hash = password => bcrypt.hash(password, parseInt(bcryptConfig.salt));
 
-exports.compare = async (password, hash) => {
-  const valid = await bcrypt.compare(password, hash);
-  return valid;
-};
+exports.compare = (password, hash) => bcrypt.compare(password, hash);
