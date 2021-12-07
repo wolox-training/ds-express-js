3303762ec09ef34997200208fe2458eb78ebe243
David Sandoval <david.sandoval@wolox.co>
2021-12-07 10:08:12 -0500

    fix suggestions

--- app/helpers/number.js
+++ app/helpers/number.js
@@ -1,7 +1,1 @@
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-
-module.exports = {
-  MIN: 1,
-  MAX: 1000,
-  getRandomInt
-};
+exports.getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
